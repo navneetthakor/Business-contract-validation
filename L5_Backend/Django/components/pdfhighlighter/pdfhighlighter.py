fe9389450e489a2d7a5268ca0c059462779fd72b
Ronakvekariya <156982950+Ronakvekariya@users.noreply.github.com>
2024-07-03 21:03:11 +0530

    Update pdfhighlighter.py

--- L5_Backend/Django/components/pdfhighlighter/pdfhighlighter.py
+++ L5_Backend/Django/components/pdfhighlighter/pdfhighlighter.py
@@ -6,23 +6,19 @@ from cloudinary.utils import cloudinary_url
 from django.conf import settings
 
 class PdfHighlighter:
-    def __init__(self, pdf_path, ner_dict):
+    def __init__(self, pdf_path, deviation_words):
         self.pdf_path = pdf_path
-        self.ner_dict = ner_dict
-        self.list = []
+        self.deviation_words = deviation_words
 
     def highlight(self):
         try:
             print("\n\n\nhighlighting user pdf.....\n")
-            
-            self.list = [key for key in self.ner_dict.keys()]
-            # print(self.list)
 
             doc = pymupdf.open(self.pdf_path)
             print(doc,"\n\n\n")
 
             for page in doc:
-                for word in self.list:
+                for word in self.deviation_words:
                     instance = page.search_for(word)
                     for inst in instance:
                         page.add_highlight_annot(inst)
